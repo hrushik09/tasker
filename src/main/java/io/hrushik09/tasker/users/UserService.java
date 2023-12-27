c8614cae20033431c3992f684359c4dc94a709d4
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2023-12-28 00:15:11 +0530

    add WebMvcTest for UserController

---
+++ src/main/java/io/hrushik09/tasker/users/UserService.java
@@ -0,0 +1,10 @@
+package io.hrushik09.tasker.users;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    public UserDTO create(CreateUserCommand cmd) {
+        return null;
+    }
+}
