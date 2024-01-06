1abf6b6570feccfbb1447a597a468f85e2e9f756
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2024-01-06 11:43:22 +0530

    add end-to-end test for fetching card details

--- src/test/java/io/hrushik09/tasker/EndToEndTestDataPersister.java
+++ src/test/java/io/hrushik09/tasker/EndToEndTestDataPersister.java
@@ -3,9 +3,7 @@ package io.hrushik09.tasker;
 import io.hrushik09.tasker.boards.BoardService;
 import io.hrushik09.tasker.boards.CreateBoardCommand;
 import io.hrushik09.tasker.boards.CreateBoardResponse;
-import io.hrushik09.tasker.cards.CardService;
-import io.hrushik09.tasker.cards.CreateCardCommand;
-import io.hrushik09.tasker.cards.CreateCardResponse;
+import io.hrushik09.tasker.cards.*;
 import io.hrushik09.tasker.lists.CreateListCommand;
 import io.hrushik09.tasker.lists.CreateListResponse;
 import io.hrushik09.tasker.lists.ListService;
@@ -62,5 +60,9 @@ public class EndToEndTestDataPersister {
         CreateBoardResponse createBoardResponse = havingPersistedBoard(createUserResponse.id());
         CreateListResponse createListResponse = havingPersistedList("Not important", createBoardResponse.id());
         return havingPersistedCard("No important", createListResponse.id());
+    }
+
+    public UpdateCardDescriptionResponse havingUpdatedCardDescription(Integer id, String description) {
+        return cardService.updateDescription(new UpdateDescriptionCommand(id, description));
     }
 }
